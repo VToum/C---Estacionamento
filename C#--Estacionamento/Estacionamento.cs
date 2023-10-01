859adbef587a574a2357d627d14635cc5fdd8060
Everton <everleao@gmail.com>
2023-10-01 00:32:23 -0300

    Adicionar arquivos de projeto.

---
+++ C#--Estacionamento/Estacionamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___Estacionamento
+{
+    public class Estacionamento
+    {
+        public string ?LicensePlate {  get; set; }
+        public decimal StartPrice { get; set; }
+        public decimal HourPrice { get; set; }
+        public bool ActiveSystem { get; set; }
+
+        List<string> vehicles = new List<string>();
+
+
+        public void addVehicle(string licensePlate)
+        {
+            vehicles.Add(licensePlate);
+        }
+
+        public void removeVehicle(string licensePlate) 
+        {
+            vehicles.Remove(licensePlate);
+        }
+        
+        public List<string> listVehicle()
+        {
+
+            foreach (var vehicle in vehicles) 
+            {
+                Console.WriteLine(vehicle);
+            }
+
+            return vehicles;
+        }
+    }
+}
---
+++ C#--Estacionamento/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using C___Estacionamento;
+
+Estacionamento esta = new Estacionamento();
+
+esta.ActiveSystem = true;
+
+esta.StartPrice = 0;
+esta.HourPrice = 0;
+string licensePlate = "";
+
+Console.WriteLine("Welcome to parking lot! \n" +
+    "Enter the value: ");
+esta.StartPrice = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Now price hour: ");
+esta.HourPrice = Convert.ToDecimal(Console.ReadLine());
+
+decimal price = 0;
+price = esta.StartPrice * esta.HourPrice;
+
+Console.WriteLine($"amount to be paid: {price}$");
+while (esta.ActiveSystem) 
+{
+    Console.WriteLine("1 - Register");
+    Console.WriteLine("2 - Remove");
+    Console.WriteLine("3 - List Vehicle");
+    Console.WriteLine("4 - Exit");
+    
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("Enter the license plate");
+            licensePlate = Convert.ToString(Console.ReadLine());
+            esta.addVehicle(licensePlate);
+            Console.WriteLine($"Register success! ");
+            break;
+        case "2":
+            Console.WriteLine("Enter the license plate");
+            licensePlate = Convert.ToString(Console.ReadLine());
+            esta.removeVehicle(licensePlate);
+            Console.WriteLine($"Remove success! ");
+            break;
+        case "3":
+            Console.WriteLine("number of vehicles licensePlate");
+            esta.listVehicle();
+            break;
+        case "4":
+            esta.ActiveSystem = false;
+            break;
+        default:
+            Console.WriteLine("Wrong choice");
+            break;
+    }
+}
